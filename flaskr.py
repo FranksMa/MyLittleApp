8bbe484f69184c17ea163e383baaf3d57e24b529
mtq1992 <1219746766@qq.com>
2018-09-02 16:35:55 +0800

    debug db

--- flaskr.py
+++ flaskr.py
@@ -3,8 +3,12 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
+DATABASE = 'db/flaskr.db'
+DEBUG = True
 
 app = Flask(__name__)
+# environment config
+app.config.from_object(__name__)
 
 
 def connect_db():
@@ -77,4 +81,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0')
