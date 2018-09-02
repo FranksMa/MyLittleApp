8c694db0092f5a2e7c817281ec460eb55d3970a0
Franks Ma <mtqmx3@gmail.com>
2018-09-02 15:53:41 +0800

    change to my mac

--- flaskr.py
+++ flaskr.py
@@ -39,6 +39,7 @@ def close_db(error):
 
 @app.route('/')
 def show_entries():
+    g.db = get_db()
     cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
